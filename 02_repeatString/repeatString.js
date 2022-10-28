03cd3c8b99d4f9aa09d8686ff32d8a4ed0a3abb2
LordThiouk <pirlothiouk@gmail.com>
2022-10-28 17:39:00 +0000

    Done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  var i = 1;
+    var str = new String();
+  do{
+        if (num === 0){
+            return "";
+        }
+        else if (num < 0){
+            return "ERROR";
+        }
+        else {
+            return str= string.repeat(num);
+        }
+    }while (i < num)
+    
 };
 
 // Do not edit below this line
