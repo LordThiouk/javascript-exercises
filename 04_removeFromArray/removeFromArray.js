65f5c47982e22154230d25e01c09b078b68d60a4
LordThiouk <pirlothiouk@gmail.com>
2022-10-31 04:18:34 +0000

    RemoveFromArray Done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr = [], ...theArgs) {
+    let Args = theArgs;
+    for(let i=0; i <= arr.length; i++ ){
+        for(let j=0; j<= Args.length; j++){
+            if ( arr[i]=== Args[j]){
+                arr = arr.filter(item => Args.indexOf(item) === -1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
